deb0e04dbd4a878aa88a7164a3b7171e9b4d3e99
Tomasz Czuba <t.czuba95@gmail.com>
2021-06-04 00:07:30 +0200

    Resolving issues with settings file for heroku app

--- oauth2/settings.py
+++ oauth2/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
-import dj-database-url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -100,8 +100,7 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-DATABASES['default'] = dj_database_url.config(
-    conn_max_age=600, ssl_require=True)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
