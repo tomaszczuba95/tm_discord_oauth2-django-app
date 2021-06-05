7ac4fe2842d4ebc5fdaddd392e202fe5acf28aff
Tomasz Czuba <t.czuba95@gmail.com>
2021-06-05 18:51:33 +0200

    Debug state loading from config file

--- oauth2/settings.py
+++ oauth2/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-#DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['tm-discord-authorization.herokuapp.com', '127.0.0.1']
 
